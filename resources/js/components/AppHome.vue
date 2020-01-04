b3468c9fd9f5e8ab9cb595b0e155de7a1c6a6e22
Malkit Singh <rgcsm.pb@gmail.com>
2020-01-05 00:27:22 +0530

    auth complete

---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,13 @@
+<template>
+
+<div class="app-home">
+    <router-view></router-view>
+</div>
+
+</template>
+
+<script>
+    export default {
+        name:"AppHome",
+    }
+</script>
---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+Route::group([
+
+
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
